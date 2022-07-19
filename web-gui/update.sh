7cdb1a18499b1e05eb01f6540db651b746b13c14
AeX03 <103602164+AeX03@users.noreply.github.com>
2022-07-19 15:04:21 +0100

    Update update.sh

--- web-gui/update.sh
+++ web-gui/update.sh
@@ -28,31 +28,7 @@ sleep 3
 
 clear
 
-sudo chmod +x /etc/
-
-clear
-
-sudo chmod +x /usr/share/doc
-
-clear
-
-sudo rm -rf /usr/share/doc/eLys/
-
-clear
-
-cd /etc/
-
-clear
-
-sudo rm -rf /etc/eLys
-
-clear
-
-mkdir eLys
-
-clear
-
-cd elys
+sudo rm -rf eLys
 
 clear
 
